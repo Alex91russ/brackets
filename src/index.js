e2d323e0058169e27ab67a7f5db0d072281b6fbe
Aleksandr Kochetkov <k_alex91@mail.ru>
2020-10-09 21:26:54 +0400

    feat: complete task Brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let newConfig = bracketsConfig.map((value) => value.join(""));
+
+    for (let i = 0; i < newConfig.length; i++) {
+        if (str.includes(newConfig[i])) {
+            str = str.replace(newConfig[i], "");
+            i = -1;
+        }
+    }
+
+    return str.length === 0;
+};
